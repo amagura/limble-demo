e3790d086112f79138aeec40fb8f616f08338b35
Alexej Magura <amagura28@gmail.com>
2022-07-29 19:03:05 -0600

    updates mysql version

--- mysql.tf
+++ mysql.tf
@@ -1,7 +1,7 @@
 resource "aws_db_instance" "default" {
   allocated_storage     = 10
   engine                = "mysql"
-  engine_version        = "5.7"
+  engine_version        = "8.0.28"
   instance_class        = "db.t3.small"
   parameter_group_name  = "default.mysql5.7"
   skip_final_snapshot   = true
