6edba21973313ba4470d5026304c32fb24955e05
Alexej Magura <amagura28@gmail.com>
2022-07-29 18:51:47 -0600

    move some things around

--- db.tf
+++ db.tf
@@ -3,10 +3,10 @@ resource "aws_db_instance" "default" {
   engine                = "mysql"
   engine_version        = "5.7"
   instance_class        = "db.t3.small"
-  db_name                  = "demodb"
-  username              = "lidem"
-  password              = "Lithuanian UnknowingsHesit ance sAmarium"
   parameter_group_name  = "default.mysql5.7"
   skip_final_snapshot   = true
   db_subnet_group_name  = module.vpc.private_subnets[0]
+  db_name               = var.db_name
+  username              = var.db_user
+  password              = var.db_pass
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,21 @@ variable "region" {
   type        = string
   default     = "us-east-2"
 }
+
+variable "db_name" {
+  description = "Project database name"
+  type        = string
+  default     = "demodb"
+}
+
+variable "db_user" {
+  description = "Project database user"
+  type        = string
+  default     = "lid"
+}
+
+variable "db_pass" {
+  description = "Project database password"
+  type        = string
+  default     = "Lithuanian UnknowingsHesit ance sAmarium"
+}
