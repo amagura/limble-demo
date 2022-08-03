82878ff547a73947a7e6266534fa3e2e4d9c9ee0
Alexej Magura <amagura28@gmail.com>
2022-08-03 14:07:01 -0600

    should work for vpc

--- mysql.tf
+++ mysql.tf
@@ -5,8 +5,13 @@ resource "aws_db_instance" "default" {
   instance_class        = "db.t3.small"
   parameter_group_name  = "default.mysql5.7"
   skip_final_snapshot   = true
-  db_subnet_group_name  = module.vpc.private_subnets[0]
+  # db_subnet_group_name  = module.vpc.private_subnets[0]
+  db_subnet_group_name  = "${module.vpc.name}"
   db_name               = var.db_name
   username              = var.db_user
   password              = var.db_pass
 }
+
+# resource "aws_db_subnet_group" "myvpc" {
+#   name                  = "k8s_vpc"
+#   subnet_ids            = [
