da6af2257315614088d95a2669f0d736f286b091
Alexej Magura <amagura28@gmail.com>
2022-08-06 17:03:17 -0600

    hopefully this fixes the parameter group issue

--- rds.tf
+++ rds.tf
@@ -1,3 +1,21 @@
+resource "aws_db_subnet_group" "_" {
+  subnet_ids  = module.vpc.private_subnets
+}
+
+resource "aws_db_parameter_group" "default" {
+  name    = "rds-mysql"
+  family  = "mysql5.7"
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8"
+  }
+  parameter {
+    name  = "character_set_client"
+    value = "utf8"
+  }
+}
+
 resource "aws_db_instance" "default" {
   allocated_storage       = 10
   engine                  = "mysql"
@@ -7,11 +25,14 @@ resource "aws_db_instance" "default" {
   skip_final_snapshot     = true
   # db_subnet_group_name  = module.vpc.private_subnets[0]
   # db_subnet_group_name  = "${module.vpc.name}"
+  db_subnet_group_name    = aws_db_subnet_group._.id
+  # parameter_group_name    = aws_db_parameter_group.default.name
   vpc_security_group_ids  = ["${aws_security_group.rds.id}"]
   db_name                 = var.db_name
   username                = var.db_user
   password                = var.db_pass
 }
+
 
 # resource "aws_db_subnet_group" "myvpc" {
 #   name                  = "k8s_vpc"
