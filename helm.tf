a6c5d5ef8fe70433b4d9aea0b8880ab04650b003
Alexej Magura <amagura28@gmail.com>
2022-08-05 20:36:26 -0600

    adds a lot of helm charts

--- helm.tf
+++ helm.tf
@@ -4,14 +4,73 @@ provider "helm" {
   }
 }
 
-resource "helm_release" "nginx_ingress" {
-  name            = "nginx-ingress"
-  repository      = "https://kubernetes.github.io/"
-  chart           = "ingress-nginx"
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts/prometheus-community/"
+  chart      = "prometheus"
+  namespace  = "watcher"
+}
+
+resource "helm_release" "alert-manager" {
+  name        = "alert-manager"
+  repository  = "https://prometheus-community.github.io/helm-charts/prometheus-community/"
+  chart       = "alert-manager"
+  namespace   = "watcher"
+}
+
+resource "helm_release" "nginx-ingress" {
+  name        = "nginx-ingress"
+  repository  = "https://kubernetes.github.io/"
+  chart       = "ingress-nginx"
+  namespace   = "nginx-ingress"
 
   set {
-    name          = "service.type"
-    value         = "ClusterIP"
+    name      = "service.type"
+    value     = "ClusterIP"
+  }
+  set {
+    name      = "controller.metrics.enabled"
+    value     = true
+  }
+  set {
+    name      = "controller.metrics.serviceMonitor.enabled"
+    value     = true
   }
 }
+
+resource "helm_release" "cert-manager" {
+  name        = "cert-manager"
+  repository  = "https://charts.jetstack.io"
+  chart       = "cert-manager"
+  version     = "v1.9.1"
+  namespace   = "cert-manager"
+  values      = [file("values/cm.yml")]
+}
+
+resource "helm_release" "npd" {
+  name        = "npd"
+  repository  = "https://charts.deliveryhero.io/"
+  chart       = "node-problem-detector"
+  namespace   = "watcher"
+
+  set {
+    name  = "metrics.enabled"
+    value = true
+  }
+}
+
+resource "helm_release" "grafana" {
+  name        = "grafana"
+  repository  = "https://grafana.github.io/helm-charts/grafana/"
+  chart       = "grafana"
+  namespace   = "watcher"
+
+
+  set {
+    name  = "serviceMonitor.enabled"
+    value = true
+  }
+}
+
+
 
