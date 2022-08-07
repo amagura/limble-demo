ba639b91fd91a29f6cfbd5c6d3a322a0cb4db2cc
Alexej Magura <amagura28@gmail.com>
2022-08-07 14:56:57 -0600

    oh my god

--- rds.tf
+++ rds.tf
@@ -2,26 +2,26 @@ resource "aws_db_subnet_group" "_" {
   subnet_ids  = module.vpc.private_subnets
 }
 
-resource "aws_db_parameter_group" "default" {
-  name    = "rds-mysql"
-  family  = "mysql5.7"
-
-  parameter {
-    name  = "character_set_server"
-    value = "utf8"
-  }
-  parameter {
-    name  = "character_set_client"
-    value = "utf8"
-  }
-}
+# resource "aws_db_parameter_group" "default" {
+#   name    = "rds-mysql"
+#   family  = "mysql5.7"
+#
+#   parameter {
+#     name  = "character_set_server"
+#     value = "utf8"
+#   }
+#   parameter {
+#     name  = "character_set_client"
+#     value = "utf8"
+#   }
+# }
 
 resource "aws_db_instance" "default" {
   allocated_storage       = 10
   engine                  = "mysql"
   engine_version          = "8.0.28"
   instance_class          = "db.t3.small"
-  parameter_group_name    = "default.mysql5.7"
+  # parameter_group_name    = "default.mysql5.7"
   skip_final_snapshot     = true
   # db_subnet_group_name  = module.vpc.private_subnets[0]
   # db_subnet_group_name  = "${module.vpc.name}"
