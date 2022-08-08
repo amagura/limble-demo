9fe9f6847875dffbe6ac355d3b3baf54e873e9e6
Alexej Magura <amagura28@gmail.com>
2022-08-07 20:33:58 -0600

    enables kube config again

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  # config_path            = "~/.kube/config"
+  config_path            = "~/.kube/config"
 }
 
 provider "kubectl" {
