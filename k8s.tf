11f97b548fed5e661cefb78567192cb02c628306
Alexej Magura <amagura28@gmail.com>
2022-08-08 11:33:21 -0600

    comments out whole file

--- k8s.tf
+++ k8s.tf
@@ -1,20 +1,18 @@
-resource "kubernetes_namespace" "watcher" {
-  metadata {
-    name = "watcher"
-  }
-}
-resource "kubernetes_namespace" "nginx-ingress" {
-  metadata {
-    name = "nginx-ingress"
-  }
-}
-resource "kubernetes_namespace" "cert-manager" {
-  metadata {
-    name = "cert-manager"
-  }
-}
-
-
+# resource "kubernetes_namespace" "watcher" {
+#   metadata {
+#     name = "watcher"
+#   }
+# }
+# resource "kubernetes_namespace" "nginx-ingress" {
+#   metadata {
+#     name = "nginx-ingress"
+#   }
+# }
+# resource "kubernetes_namespace" "cert-manager" {
+#   metadata {
+#     name = "cert-manager"
+#   }
+# }
 # resource "kubectl_manifest" "certissuer" {
 #   yaml_body = file("certissuer.yaml")
 # }
