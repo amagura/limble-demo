8c4e8d3a294c1bbea54ff7435d84e4bdd616d43d
Alexej Magura <amagura28@gmail.com>
2022-08-09 12:02:31 -0600

    no more helm

--- main.tf
+++ main.tf
@@ -8,19 +8,13 @@
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  config_path            = "~/.kube/config"
+  # config_path            = "~/.kube/config"
 }
 
 provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  config_path            = "~/.kube/config"
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
+  # config_path            = "~/.kube/config"
 }
 
 provider "aws" {
